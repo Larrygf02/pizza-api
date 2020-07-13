acab6971b24f6c5ed5a57c9c93fdb64988fa0714
Larrygf02 <raulgf_02@hotmail.com>
2020-07-12 23:33:16 -0500

    Add handlers

--- api.js
+++ api.js
@@ -1,7 +1,14 @@
 'use strict'
-const getPizzas = require('./handlers/get-pizzas')
 const Api = require('claudia-api-builder')
 const api = new Api() 
+
+const getPizzas = require('./handlers/get-pizzas')
+
+api.get('/', () => 'Welcome to Pizza Api')
+
+api.get('/pizzas', () => {
+    return getPizzas()
+})
 
 api.get('/pizzas/{id}', (request) => {
     return getPizzas(request.pathParams.id)
