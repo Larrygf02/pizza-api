0909f5732b8ff2dcc815df75368a70b6a85bd144
Larrygf02 <raulgf_02@hotmail.com>
2020-07-12 23:16:53 -0500

    get pizzas

---
+++ handlers/get-pizzas.js
@@ -0,0 +1,11 @@
+const pizzas = require('../data/pizzas.json')
+
+function getPizzas(pizzaId) {
+    if (!pizzaId) {
+        return pizzas
+    }
+    const pizza = pizzas.find(pizza => pizza.id == pizzaId)
+    if (pizza) return pizza
+    throw new Error('The pizza you requested not found')
+}
+module.exports = getPizzas
