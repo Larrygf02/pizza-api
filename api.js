fe3286beec233614821d80999e7d80b62878e331
Larrygf02 <raulgf_02@hotmail.com>
2020-07-12 23:31:23 -0500

    module get-pizzas

--- api.js
+++ api.js
@@ -1,14 +1,12 @@
 'use strict'
+const getPizzas = require('./handlers/get-pizzas')
 const Api = require('claudia-api-builder')
-const api = new Api()
+const api = new Api() 
 
-api.get('/pizzas', () => {
-    return [
-        'Capricciosa',
-        'Quattro Formaggi',
-        'Napoletana',
-        'Margherita'
-    ]
+api.get('/pizzas/{id}', (request) => {
+    return getPizzas(request.pathParams.id)
+},{
+    error: 404
 })
 
-module.exports = api 
+module.exports = api
