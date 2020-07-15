bc127f32a61fcbfbfe913ec9f33c9fe9bef1863c
Larrygf02 <raulgf_02@hotmail.com>
2020-07-14 21:10:37 -0500

    Add dynamobd

--- handlers/create-order.js
+++ handlers/create-order.js
@@ -1,8 +1,18 @@
+const AWS = require('aws-sdk')
+const docClient = new AWS.DynamoDB.DocumentClient()
 
-function createOrder(order) {
-    if (!order || !order.pizzaId || !order.address) {
+function createOrder(request) {
+    if (!request || !request.pizzaId || !request.address) {
         throw new Error('To order pizza please provide pizza type and address')
     }
-    return {}
+    return docClient.put({
+        TableName: 'pizza-orders',
+        Item: {
+            orderId: 'some-id',
+            pizza: request.pizza,
+            address: request.address,
+            orderStatus: 'pending'
+        }
+    }).promise()
 }
 module.exports = createOrder
