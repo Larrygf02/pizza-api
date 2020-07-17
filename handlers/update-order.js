e2b9b04473e15afe153d49d49c0f7889b42b77b8
Larrygf02 <raulgf_02@hotmail.com>
2020-07-16 21:26:43 -0500

    Update order pizza

--- handlers/update-order.js
+++ handlers/update-order.js
@@ -1,7 +1,26 @@
-function updateOrder(id, updates) {
-    if (!id || !updates) {
+const AWS = require('aws-sdk')
+const docClient = new AWS.DynamoDB.DocumentClient()
+
+function updateOrder(id, body) {
+    if (!id || !body || !body.pizza || !body.address) {
         throw new Error('Order ID and updates object are required for updating the order')
     }
+    return docClient.update({
+        TableName: 'pizza-orders',
+        Key: {
+            orderId: id
+        },
+        UpdateExpression: "set pizza = :p, address= :a",
+        ExpressionAttributeValues: {
+            ":p": body.pizza,
+            ":a": body.address
+        },
+        ReturnValues: 'ALL_NEW'
+    }).promise()
+    .then(result => result.Attributes)
+    .catch(error => {
+        throw error
+    })
     return {
         message: `Order ${id} was successfully updated`
     }
